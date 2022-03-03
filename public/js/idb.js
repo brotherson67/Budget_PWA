e416832524d67a055ca26a53e3048382ec4a54a2
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-02 22:38:50 -0700

    added checkdb function to idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,14 +6,27 @@ request.onupgradeneeded = function (event) {
   db.createObjectStore("new_expense", { autoIncrement: true });
 };
 
+function checkDatabase() {
+  const transaction = db.transaction("new_expense", "readonly");
+  const store = transaction.objectStore("new_expense");
+  const getAll = store.getAll();
+}
 request.onsuccess = function (event) {
   db = event.target.result;
 
   if (navigator.onLine) {
-    // uploadPizza();
+    checkDatabase();
   }
 };
 
 request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_expense"], "readwrite");
+
+  const expenseObjectStore = transaction.objectStore("new_expense");
+
+  expenseObjectStore.add(record);
+}
