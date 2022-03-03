4b5e6a1fce7975ea76fc2c528700db4c1cd48263
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-02 23:22:18 -0700

    added fetch eventListener

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,22 @@
 const CACHE_NAME = "static-files-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = ["/", "/index.html"];
 
 self.addEventListener("install", (evt) => {
   console.log("Service worker installed");
+
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (evt) => {
   console.log("service worker activated");
 });
+
+self.addEventListener("fetch", (evt) => {});
